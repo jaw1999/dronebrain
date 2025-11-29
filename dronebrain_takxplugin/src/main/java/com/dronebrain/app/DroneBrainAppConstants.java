6fadbc8e73538a59c2a6805b776d99a184eb57b3
jaw1999 <110297747+jaw1999@users.noreply.github.com>
2025-11-29 02:02:21 +0100

    Remove dronebrain_takxplugin (should be gitignored)

--- dronebrain_takxplugin/src/main/java/com/dronebrain/app/DroneBrainAppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.rivet.app;
-
-public class RivetAppConstants
-{
-    public static final String FAMILY = "com.rivet";
-    public static final String TYPE = "Rivet App";
-    public static final String VERSION = "1.0.0";
-}
